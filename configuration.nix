4086f36e130711438c726a50af6cc387880fa792
TeaJhay <TeaJhay@outlook.com.au>
2026-08-28 21:00:05 +1000

    Fix nested repository structure and add NixOS config

---
+++ configuration.nix
@@ -0,0 +1,166 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, inputs, ... }:
+
+
+{
+  # import hardware-configuration (partitions) and other configs.
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  # Enable nix command flakes
+  nix.settings = {
+    experimental-features = [ 
+      nix-command 
+      flakes
+    ];
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    # Required so non-root users are allowed to use the above substituter/keys.
+    # Use @wheel for all sudo users, or list your username explicitly.
+    trusted-users = ["root" "@wheel"];
+  };
+
+  programs.noctalia-greeter = {
+    enable = true;
+    settings = {
+      session.default = "hyprland";
+    };
+  };
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use zen OR latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_zen;
+  
+  # Define your hostname.
+  networking.hostName = "NixOSVM"; 
+
+  # Configure network connections interactively with nmcli or nmtui.
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Australia/Brisbane";
+
+  # Enable sound.
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.teajhay = {
+    isNormalUser = true;
+    description = "Teejay Anderson";
+    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+      tree
+    ];
+  };
+
+  # programs.firefox.enable = true;
+
+  # List packages installed in system profile.
+  # You can use https://search.nixos.org/ to find more packages (and options).
+  environment.systemPackages = with pkgs; [
+    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    git
+    ripgrep
+    fd
+    unzip
+    nodejs
+    gcc
+    python3
+    gnumake
+    kitty
+    tuigreet
+    greetd
+    gh
+    unstable.noctalia
+    unstable.noctalia-greeter
+  ];
+
+
+
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+      PasswordAuthentication = true;
+      PermitRootLogin = "yes";
+      KbdInteractiveAuthentication = true;
+      };
+  };
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "26.05"; # Did you read the comment?
+  
+  # Enable supported filesystems
+  boot.supportedFilesystems = [ "btrfs" ];
+  
+  # Enable Hyprland
+  programs.hyprland = {
+    enable = true;
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    portalPackage = pkgs.unstable.xdg-desktop-portal-hyprland;
+    withUWSM = false;
+    xwayland.enable = true;
+  };
+
+  # Optional, hint Electron apps to use Wayland:
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+
+
+}
+
---
+++ flake.nix
@@ -0,0 +1,89 @@
+{
+  description = "My NixOS config";
+
+ 
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05"; # NixOS release channel
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable"; # NixOS unstable channel
+    nixpkgs-master.url = "github:NixOS/nixpkgs/master";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master"; # NixOS hardware channel
+    noctalia-greeter = {
+      url = "github:noctalia-dev/noctalia-greeter";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hjem = {
+      url = "github:feel-co/hjem";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    update = {
+      url = "github:ryantm/nixpkgs-update";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-monitored = {
+      url = "github:ners/nix-monitored";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixd = {
+      url = "github:nix-community/nixd";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    fan-controller = {
+      url = "github:Krutonium/BetterFanController";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+#    nur = {
+#      url = "github:nix-community/NUR";
+#      # inputs.nixpkgs.follows = "nixpkgs"; NUR does not.
+#    };
+    chaotic = {
+      url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+    };
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+  };
+
+  # Replaced long destructuring with clean inputs mapping
+outputs = inputs@{ nixpkgs, nixpkgs-unstable, nixpkgs-master, chaotic, ... }:
+    let
+      system = "x86_64-linux";
+
+      # Generic helper: turn a nixpkgs-like flake input into an overlay
+      # that exposes it as pkgs.<name>
+      mkChannelOverlay = name: flakeInput: final: prev: {
+        ${name} = import flakeInput {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      };
+
+      overlay-unstable = mkChannelOverlay "unstable" nixpkgs-unstable;
+      overlay-master   = mkChannelOverlay "master" nixpkgs-master;
+
+      # Chaotic-Nyx isn't its own nixpkgs — it's an *overlay* meant to sit on
+      # top of nixpkgs-unstable. So we build a pkgs set with their overlay
+      # applied, then namespace the whole thing under pkgs.chaotic
+      overlay-chaotic = final: prev: {
+        chaotic = import nixpkgs-unstable {
+          inherit system;
+          config.allowUnfree = true;
+          overlays = [ chaotic.overlays.default ];
+        };
+      };
+    in {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit inputs; };
+        modules = [
+          {
+            nixpkgs.overlays = [
+              overlay-unstable
+              overlay-master
+              overlay-chaotic
+            ];
+          }
+          inputs.noctalia-greeter.nixosModules.default
+          ./configuration.nix
+        ];
+      };
+    };
+}
+
