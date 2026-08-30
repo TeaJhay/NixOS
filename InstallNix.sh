432e8b97101ae5fb43199c343e96b7ada86ac8d8
TeaJhay <TeaJhay@outlook.com.au>
2026-08-30 21:24:20 +1000

    Dont care to comment

--- InstallNix.sh
+++ InstallNix.sh
@@ -13,9 +13,9 @@ if [ -e "$TARGET_FILE" ] || [ -L "$TARGET_FILE" ]; then
         echo "Refusing to remove $TARGET_FILE: it's a directory, not a file" >&2
         exit 1
     fi
+    sudo nixos-generate-config --no-filesystems --force
+    mv "$TARGET_DIR"/hardware-configuration.nix "$SRC_DIR"
     rm -f -- "$TARGET_DIR"/*
-    sudo nixos-generate-config --no-filesystems --dir "$SRC_DIR"
-
 fi
 
 # 2. Symlink everything from the current dir (except this script) into /etc/xx,
