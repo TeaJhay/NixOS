b7c35a95bb04e979f0aeea8adecdc1d2ae16f184
TeaJhay <TeaJhay@outlook.com.au>
2026-08-30 12:12:22 +1000

    maybe good

--- Deploy.sh
+++ Deploy.sh
@@ -2,10 +2,16 @@
 set -euo pipefail
 LOCAL_DIR="$(cd "$(dirname "$(realpath "$0")")" && pwd)"
 
+# Trust the local dir
 sudo git config --global --add safe.directory "$LOCAL_DIR"
+# Increase allowed mem/space for building
+mount -o remount,size=10G,noatime /nix/.rw-store
 
+# Disk to format
 DISK="/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi0"  # match disko.nix
 
+
+#script for install
 echo "About to WIPE and install to: $DISK"
 lsblk -o NAME,SIZE,MODEL "$(readlink -f "$DISK")"
 read -rp "Type YES to continue: " confirm
@@ -17,6 +23,8 @@ sudo nix --show-trace --extra-experimental-features "nix-command flakes" run git
   --mode format \
   --write-efi-boot-entries 
 
+
+# creating snapshopt
 echo "Creating pristine @void-blank snapshot..."
 sudo mount -o subvol=/ /dev/disk/by-partlabel/disk-main-root /mnt
 sudo btrfs subvolume snapshot -r /mnt/@void /mnt/@void-blank
