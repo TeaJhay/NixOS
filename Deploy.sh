d8772dae531429d72a352eeecfb4bb740f4c0c89
TeaJhay <TeaJhay@outlook.com.au>
2026-08-30 12:44:02 +1000

    maybe good

--- Deploy.sh
+++ Deploy.sh
@@ -34,8 +34,6 @@
 
 
 # Because Disko hasn't decided to resolve an issue that occurs when you don't have enough tmpfs memory or storage, I'll two phase it. However I want 1 phase at some point.
-#
-# THIS ALSO MEANS $DISK NEEDS TO BE IN YOUR Disko.nix AS Disko DOESN'T ACCEPT THE --disk FLAG
 
 #!/usr/bin/env bash
 set -euo pipefail
@@ -43,7 +41,7 @@ set -euo pipefail
 LOCAL_DIR="$(cd "$(dirname "$(realpath "$0")")" && pwd)"
 sudo git config --global --add safe.directory "$LOCAL_DIR"
 
-# DISK="/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi0"  # match disko.nix
+DISK="/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi0"  # match disko.nix
 
 echo "About to WIPE and install to: $DISK"
 lsblk -o NAME,SIZE,MODEL "$(readlink -f "$DISK")"
@@ -54,7 +52,7 @@ echo "== Phase 1: partition + format only =="
 sudo nix --extra-experimental-features "nix-command flakes" run \
   github:nix-community/disko/latest -- \
   --mode destroy,format,mount \
-  --disk main "$DISK" \
+#  --disk main "$DISK" \
   "$LOCAL_DIR/disko.nix"
 
 echo "== Enabling swap for the build step =="
@@ -65,8 +63,8 @@ echo "== Phase 2: build + install (swap already active) =="
 sudo nix --extra-experimental-features "nix-command flakes" run \
   github:nix-community/disko/latest#disko-install -- \
   --flake "$LOCAL_DIR#nixos" \
-  --mode $disko \
-#  --disk main "$DISK" \
+  --mode mount \
+  --disk main "$DISK" \
   --write-efi-boot-entries
 
 echo "== Creating pristine @void-blank snapshot =="
