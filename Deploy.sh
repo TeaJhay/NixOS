d13770e80f194d10a5875f9fcd296ca9de936b6f
TeaJhay <TeaJhay@outlook.com.au>
2026-08-30 14:11:03 +1000

    maybe good

--- Deploy.sh
+++ Deploy.sh
@@ -82,7 +82,8 @@ echo "== Phase 2: build + install (swap already active) =="
 sudo nix --extra-experimental-features "nix-command flakes" --accept-flake-config --show-trace run \
   github:nix-community/disko/latest#disko-install -- \
   --flake "$LOCAL_DIR#nixos" \
-  --option max-jobs 2 \ 
+#  --option max-jobs 2 \
+  --option store "local?root=/mnt" \
   --mode mount \
   --disk main "$DISK" \
   --write-efi-boot-entries
