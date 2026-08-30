32f080f9f46f5e584d39d6cedc6e9134ccfc25e4
TeaJhay <TeaJhay@outlook.com.au>
2026-08-30 20:05:45 +1000

    Dont care to comment

--- ephemera.nix
+++ ephemera.nix
@@ -3,51 +3,37 @@
 #       │       impermanence      │
 #       └─────────────────────────┘
 
-  boot.initrd.systemd = {
-    enable = true; # this enabled systemd support in stage1 - required for the below setup
-    services.rollback = {
-      description = "Rollback BTRFS root subvolume to a pristine state";
-      wantedBy = [ "initrd.target" ];
-      before = [ "sysroot.mount" ]; # or whatever mounts depend on it
-      after = [ "systemd-udev-settle.service" ];
-      requires = [ "systemd-udev-settle.service" ];
-      # or, more targeted:
-      unitConfig.DefaultDependencies = "no";
-      serviceConfig.Type = "oneshot";
-      script = ''
-        set -euo pipefail
-        mkdir -p /mnt
+boot.initrd.systemd = {
+  enable = true;
+  services.rollback = {
+    description = "Rollback BTRFS root subvolume to a pristine state";
+    wantedBy = [ "initrd.target" ];
+    before = [ "sysroot.mount" ];
+    after = [ "dev-disk-by\\x2dpartlabel-disk\\x2dmain\\x2droot.device" ];
+    requires = [ "dev-disk-by\\x2dpartlabel-disk\\x2dmain\\x2droot.device" ];
+    unitConfig.DefaultDependencies = "no";
+    serviceConfig.Type = "oneshot";
+    script = ''
+      set -euo pipefail
+      mkdir -p /mnt
+      mount -t btrfs -o rw,subvol=/ /dev/disk/by-partlabel/disk-main-root /mnt
 
-        # Mount the BTRFS top-level (subvolid=5), not @void itself,
-        # so we can see @void and @void-blank as siblings and
-        # delete/recreate one from the other.
-        #mount -t btrfs -o subvol=/ /dev/disk/by-partlabel/disk-main-root /mnt
-        mount -o rw,subvol=/ /dev/disk/by-partlabel/disk-main-root /mnt
+      btrfs subvolume list -o /mnt/@void |
+        cut -f9 -d' ' |
+        while read -r subvolume; do
+          echo "deleting /$subvolume subvolume..."
+          btrfs subvolume delete "/mnt/$subvolume"
+        done &&
+        echo "deleting /@void subvolume..." &&
+        btrfs subvolume delete /mnt/@void
 
-        # While we're tempted to just delete @void and create
-        # a new snapshot from @void-blank, @void is already
-        # populated at this point with a number of nested subvolumes
-        # (e.g. /var/lib/portables, /var/lib/machines), which makes
-        # `btrfs subvolume delete` fail on the parent.
-        # So, we remove them first.
-        btrfs subvolume list -o /mnt/@void |
-          cut -f9 -d' ' |
-          while read -r subvolume; do
-            echo "deleting /$subvolume subvolume..."
-            btrfs subvolume delete "/mnt/$subvolume"
-          done &&
-          echo "deleting /@void subvolume..." &&
-          btrfs subvolume delete /mnt/@void
+      echo "restoring blank /@void subvolume..."
+      btrfs subvolume snapshot /mnt/@void-blank /mnt/@void
 
-        echo "restoring blank /@void subvolume..."
-        btrfs subvolume snapshot /mnt/@void-blank /mnt/@void
-
-        # Once we're done rolling back to a blank snapshot,
-        # we can unmount /mnt and continue on the boot process.
-        umount /mnt
-      '';
-    };
+      umount /mnt
+    '';
   };
+};
 #       ┌─────────────────────────┐
 #       │       Preservation      │
 #       └─────────────────────────┘
