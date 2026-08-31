f5c9f1ee657748a4d01efeeca3853ab73fc33404
TeaJhay <140999577+TeaJhay@users.noreply.github.com>
2026-08-31 13:19:46 +1000

    test

--- ephemera.nix
+++ ephemera.nix
@@ -12,31 +12,31 @@
         "usb_storage"
         "sd_mod"
       ];
-      systemd.services.rollback-void = {
-        after = [ "dev-disk-by\\x2dpartlabel-disk\\x2dmain\\x2droot.device" ];
-        before = [ "sysroot.mount" ];
-        description = "Roll back @void root subvolume to blank snapshot";
-        path = [
-          pkgs.btrfs-progs
-          pkgs.coreutils
-          pkgs.gawk
-          pkgs.util-linux
-        ];
-        script = ''
-          mkdir -p /mnt
-          mount /dev/disk/by-partlabel/disk-main-root /mnt
-          btrfs subvolume snapshot /mnt/@void /mnt/@snapshots/@boot-$(date +%Y-%m-%d_%H-%M-%S)
-          for sub in $(btrfs subvolume list -o /mnt/@void 2>/dev/null | awk '{print $NF}' | sort -r); do
-            btrfs subvolume delete "/mnt/$sub" || true
-          done
-          btrfs subvolume delete /mnt/@void
-          btrfs subvolume snapshot /mnt/@void-blank /mnt/@void
-          umount /mnt
-        '';
-        serviceConfig.Type = "oneshot";
-        unitConfig.DefaultDependencies = "no";
-        wantedBy = [ "initrd.target" ];
-      };
+      #systemd.services.rollback-void = {
+       # after = [ "dev-disk-by\\x2dpartlabel-disk\\x2dmain\\x2droot.device" ];
+       # before = [ "sysroot.mount" ];
+      #  description = "Roll back @void root subvolume to blank snapshot";
+      #  path = [
+      #    pkgs.btrfs-progs
+      #    pkgs.coreutils
+      #    pkgs.gawk
+      #    pkgs.util-linux
+      #  ];
+      #  script = ''
+      #    mkdir -p /mnt
+      #    mount /dev/disk/by-partlabel/disk-main-root /mnt
+      #    btrfs subvolume snapshot /mnt/@void /mnt/@snapshots/@boot-$(date +%Y-%m-%d_%H-%M-%S)
+      #    for sub in $(btrfs subvolume list -o /mnt/@void 2>/dev/null | awk '{print $NF}' | sort -r); do
+      #      btrfs subvolume delete "/mnt/$sub" || true
+      #    done
+      #    btrfs subvolume delete /mnt/@void
+      #    btrfs subvolume snapshot /mnt/@void-blank /mnt/@void
+      #    umount /mnt
+       # '';
+        #serviceConfig.Type = "oneshot";
+        #unitConfig.DefaultDependencies = "no";
+        #wantedBy = [ "initrd.target" ];
+      #};
     };
   };
 #       ┌─────────────────────────┐
