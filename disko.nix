bd66d23b7725bbd886453f25584264a13a53e601
TeaJhay <140999577+TeaJhay@users.noreply.github.com>
2026-08-31 12:35:48 +0000

    Connah.nix

--- disko.nix
+++ disko.nix
@@ -1,72 +1,78 @@
-{
-  fileSystems."/nix".neededForBoot = true;
-  fileSystems."/persistent".neededForBoot = true; # Required for impermanence persistence
-
- # Below if going for ephemeral
- # disko.devices.nodev = {
- #   "/" = {
- #     fsType = "tmpfs";
- #     mountOptions = [
- #       "size=25%"
- #       "mode=755"
- #     ];
- #   };
- # };
-
+{ inputs, ... }: {
+  imports = [ inputs.disko.nixosModules.disko ];
   disko.devices.disk.main = {
+    content = {
+      partitions = {
+        esp = {
+          content = {
+            format = "vfat";
+            mountOptions = [ "umask=0077" ];
+            mountpoint = "/boot";
+            type = "filesystem";
+          };
+          name = "ESP";
+          priority = 1;
+          size = "5G";
+          type = "EF00";
+        };
+        root = {
+          content = {
+            extraArgs = [ "-f" ];
+            subvolumes = {
+              "/@nix" = {
+                mountOptions = [
+                  "subvol=@nix"
+                  "compress=zstd"
+                  "noatime"
+                ];
+                mountpoint = "/nix";
+              };
+              "/@persistent" = {
+                mountOptions = [
+                  "subvol=@persistent"
+                  "compress=zstd"
+                  "noatime"
+                ];
+                mountpoint = "/persistent";
+              };
+              "/@snapshots" = {
+                mountOptions = [ "subvol=@snapshots" ];
+                mountpoint = "/snapshots";
+              };
+              "/@void" = {
+                mountOptions = [
+                  "subvol=@void"
+                  "compress=zstd"
+                  "noatime"
+                ];
+                mountpoint = "/";
+              };
+              "/@void-blank" = {
+                mountOptions = [ "subvol=@void-blank" ];
+              };
+            };
+            type = "btrfs";
+          };
+          name = "root";
+          priority = 3;
+          size = "100%";
+        };
+        swap = {
+          content = {
+            resumeDevice = true;
+            type = "swap";
+          };
+          name = "swap";
+          priority = 2;
+          size = "12G";
+        };
+      };
+      type = "gpt";
+    };
     device = "/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi0";
 #    device = "/dev/disk/by-id/nvme-CT1000T500SSD8_242649A0A283";
     type = "disk";
-    content.type = "gpt";
-    content.partitions.esp = {
-      name = "ESP";
-      priority = 1;
-      size = "1G";
-      type = "EF00";
-      content = {
-        type = "filesystem";
-        format = "vfat";
-        mountpoint = "/boot";
-      };
-    };
-    content.partitions.root = {
-      name = "root";
-      priority = 3;
-      size = "100%";
-      content = {
-        extraArgs = [ "-f" ];
-        type = "btrfs";
-        subvolumes = {
-          "/@nix" = {
-            mountpoint = "/nix";
-            mountOptions = [ "compress=zstd" "noatime" ];
-          };
-          "/@persistent" = {
-            mountpoint = "/persistent";
-            mountOptions = [ "compress=zstd" "noatime" ];
-          };
-          "/@snapshots" = {
-            mountpoint = "/snapshots";
-          };
-          "/@home" = {
-            mountpoint = "/persistent/home";
-            mountOptions = [ "compress=zstd" ];
-          };
-          "/@void" = {
-            mountpoint = "/";
-            mountOptions = [ "compress=zstd" "noatime" ];
-          };
-        };
-      };
-    };
-    content.partitions.swap = {
-      name = "SWAP";
-      priority = 2;
-      size = "12G";
-      content = {
-        resumeDevice = true;
-        type = "swap";
-      };
-    };
   };
+  fileSystems."/nix".neededForBoot = true;
+  fileSystems."/persistent".neededForBoot = true;
 }
