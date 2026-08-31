5601018d019092d584ec6eee7c8bba1e36e23e7a
TeaJhay <140999577+TeaJhay@users.noreply.github.com>
2026-08-31 12:56:21 +0000

    Connors but intel

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -7,9 +7,40 @@
   imports =
     [ (modulesPath + "/profiles/qemu-guest.nix")
     ];
-
-  boot.initrd.availableKernelModules = [ "uhci_hcd" "ehci_pci" "ahci" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
-  boot.initrd.kernelModules = [ ];
+boot = {
+    initrd = {
+      availableKernelModules = [
+        "nvme"
+        "xhci_pci"
+        "ahci"
+        "usb_storage"
+        "sd_mod"
+      ];
+      systemd.services.rollback-void = {
+        after = [ "dev-disk-by\\x2dpartlabel-disk\\x2dmain\\x2droot.device" ];
+        before = [ "sysroot.mount" ];
+        description = "Roll back @void root subvolume to blank snapshot";
+        path = [
+          pkgs.btrfs-progs
+          pkgs.coreutils
+          pkgs.gawk
+          pkgs.util-linux
+        ];
+        script = ''
+          mkdir -p /mnt
+          mount /dev/disk/by-partlabel/disk-main-root /mnt
+          for sub in $(btrfs subvolume list -o /mnt/@void 2>/dev/null | awk '{print $NF}' | sort -r); do
+              btrfs subvolume delete "/mnt/$sub" || true
+          done
+          btrfs subvolume delete /mnt/@void
+          btrfs subvolume snapshot /mnt/@void-blank /mnt/@void
+          umount /mnt
+        '';
+        serviceConfig.Type = "oneshot";
+        unitConfig.DefaultDependencies = "no";
+        wantedBy = [ "initrd.target" ];
+      };
+    };
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
